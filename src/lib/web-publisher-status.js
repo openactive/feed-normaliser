6ed7751a2436214760eea4b2dde85109b4d42fa3
Michael Wood <michael.wood@opendataservices.coop>
2020-06-24 18:28:50 +0100

    web-publisher-status: Add logo and license to status output

--- src/lib/web-publisher-status.js
+++ src/lib/web-publisher-status.js
@@ -3,7 +3,16 @@ import { database_pool } from './database.js';
 class PublisherStatus {
   static async getLatestInfo() {
     const client = await database_pool.connect();
-    const res = await client.query("SELECT name, url from publisher");
+    const sqlQuery = `
+     SELECT
+        name,
+        url,
+        data->'publisher'->'logo'->'url' as "logoUrl",
+        data->'license' as license
+      FROM publisher
+    `;
+
+    const res = await client.query(sqlQuery);
     client.release();
 
     return res.rows;
