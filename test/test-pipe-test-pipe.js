cb2ad088e68c299f56d2979658cbf3a55bb5d20e
James <james.baster@opendataservices.coop>
2020-06-24 14:05:58 +0100

    tests: mocha fix; move code inside it()

--- test/test-pipe-test-pipe.js
+++ test/test-pipe-test-pipe.js
@@ -9,24 +9,20 @@ import TestPipe from '../src/lib/pipes/test-pipe.js';
  ***/
 
 describe('test-pipe', function() {
-    describe('basic test', function() {
+    it('basic test', function(done) {
 
         let pipe = new TestPipe({id: 67, data: {"test":true }}, []);
         let results_promise = pipe.run();
         results_promise.then((results)=> {
 
-            it('should have 2 results', function() {
               assert.equal(results.length,2);
-            });
-
-            it('check result 1', function() {
               assert.deepEqual(results[0].data,{ test: 1, data: { test: true } });
-            });
-
-            it('check result 2', function() {
               assert.deepEqual(results[1].data,{ test: 2, data: { test: true } });
-            });
 
+        })
+        .then(() => done(), done)
+        .catch((error) => {
+            done(error);
         });
     });
 });
