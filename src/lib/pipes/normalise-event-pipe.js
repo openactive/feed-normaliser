e527ae1ee5729feac7c864fd672ab5c5d2816215
rhiaro <amy@rhiaro.co.uk>
2020-06-25 14:47:35 +0200

    pipes: normalise Event pipe

---
+++ src/lib/pipes/normalise-event-pipe.js
@@ -0,0 +1,19 @@
+import Pipe from './pipe.js';
+import NormalisedEvent from '../normalised-event.js';
+
+// Normalises an object with a type of Event
+class NormaliseEventPipe extends Pipe {
+  run(){
+    return new Promise(async resolve => {
+
+      let normalisedEvent1 = new NormalisedEvent({
+        }, "Event");
+
+      this.normalisedEvents.push(normalisedEvent1);
+
+      resolve(this.normalisedEvents);
+    });
+  }
+}
+
+export default NormaliseEventPipe;
