e64d224a145080863891a234433b8e1c81cb3b9a
rhiaro <amy@rhiaro.co.uk>
2020-07-09 16:18:30 +0200

    pipes: Process events with subEvents

--- src/lib/pipes/normalise-event-pipe.js
+++ src/lib/pipes/normalise-event-pipe.js
@@ -1,4 +1,5 @@
 import Pipe from './pipe.js';
+import Utils from '../utils.js';
 import NormalisedEvent from '../normalised-event.js';
 
 // Normalises an object with a type of Event
@@ -6,34 +7,60 @@ class NormaliseEventPipe extends Pipe {
   run(){
     return new Promise(async resolve => {
 
-        this.doCleanup();
-        let type = this.rawData.type;
-
         console.log(`Running ${this.rawData.id} (${this.rawData.type}) through ${this.constructor.name}`);
+        let type = this.getType();
+        let kind = this.getKind();
 
         if (type == 'Event' || type == 'OnDemandEvent'){
             // The top level event is the Event
 
-            let processedEvent = this.parseEvent(this.rawData);
-            let normalisedEvent = new NormalisedEvent(processedEvent, "Event");
-
+            this.doCleanup();
+            let normalisedEvent = new NormalisedEvent(this.rawData, kind);
             this.normalisedEvents.push(normalisedEvent);
 
-            // TODO: in theory regular Events might have subEvent or superEvent
+        }
+        else if (typeof this.rawData.subEvent !== 'undefined'){
+            // There are subEvents. If any are Event type, we normalise them.
+            // This is expected to catch EventSeries and HeadlineEvent
+            // TODO: if an Event has subEvents do we throw away the parent?
+            //       in the same way we do for other types?
 
-        }else{
-            // The top level event is something else, but it has subEvents
-            // so we're converting them into flat Events
+            let eventSubEvents = false;
+            let subEvents = Utils.ensureArray(this.rawData.subEvent);
+
+            for (let subEvent of subEvents){
+                let type = this.getType(subEvent);
+                if(type == "Event" || type == "OnDemandEvent"){
+                    // We only want to continue processing if any of the subEvents
+                    // are the right type
+                    eventSubEvents = true;
+                    break;
+                }
+            }
+
+            if (eventSubEvents){
+                this.doCleanup();
+                let {subEvent, ...parentEvent} = this.rawData;
+                parentEvent.type = "Event";
+
+                // Combine parent event data with each subEvent to make NormalisedEvents
+                // Any properties that are on both the subEvent and the parent will take the
+                // value from the subEvent
+                for (let sub of subEvent){
+                    let normalisedEventData = {
+                        ...parentEvent,
+                        ...sub
+                    }
+                    let normalisedEvent = new NormalisedEvent(normalisedEventData, kind);
+                    this.normalisedEvents.push(normalisedEvent);
+                }
+            }
         }
 
         resolve(this.normalisedEvents);
     });
   }
 
-  parseEvent(eventData, parentEvent){
-    // TODO
-    return eventData;
-  }
 }
 
 export default NormaliseEventPipe;
