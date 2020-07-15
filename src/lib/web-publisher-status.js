5c18e59163feea666ba185c6b6dca3dd7c95c2f2
Michael Wood <michael.wood@opendataservices.coop>
2020-07-15 18:29:13 +0100

    lib: web-publisher-status: Add a feed count to the JSON output

--- src/lib/web-publisher-status.js
+++ src/lib/web-publisher-status.js
@@ -3,18 +3,21 @@ import { database_pool } from './database.js';
 class PublisherStatus {
   static async getLatestInfo() {
     const client = await database_pool.connect();
-    const sqlQuery = `
+    const publisherSqlQuery = `
      SELECT
-        name,
-        url,
-        data->'publisher'->'logo'->'url' as "logoUrl",
-        data->'license' as license
+        publisher.name,
+        publisher.url,
+        publisher.data->'publisher'->'logo'->'url' as "logoUrl",
+        publisher.data->'license' as license,
+        COUNT(publisher_feed.id) as "feedCount"
       FROM publisher
+      LEFT JOIN publisher_feed on publisher.id=publisher_feed.publisher_id
+      GROUP BY publisher.id
     `;
 
-    const res = await client.query(sqlQuery);
-    client.release();
+    const res = await client.query(publisherSqlQuery);
 
+    client.release();
     return res.rows;
   }
 }
