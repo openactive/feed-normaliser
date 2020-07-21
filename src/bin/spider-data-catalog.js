8cee9db13eac90faca1f4fd08b988f4c292e2bb4
James <james.baster@opendataservices.coop>
2020-07-21 13:58:30 +0100

    tls: Set Node to support TLs v1.0
    https://github.com/openactive/conformance-services/issues/2

--- src/bin/spider-data-catalog.js
+++ src/bin/spider-data-catalog.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import spider from '../lib/spider.js';
 import Settings from '../lib/settings.js';
+import tls from 'tls';
+
+tls.DEFAULT_MIN_VERSION = Settings.tlsDefaultMinimumVersion;
 
 spider(Settings.spiderDataCatalogStartURL);
 
