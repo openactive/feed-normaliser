a094b147cc8eaaa88045ecea179f605dd6cde049
Michael Wood <michael.wood@opendataservices.coop>
2020-07-21 17:36:56 +0100

    web-publisher-info: Add feed validation metrics
    This adds two metrics to the publisher info:
     - validPercent (how many data items passed)
     - validationDonePercent (how much of the validation has completed)
    
     validationDonePercent is intended to allow consumers such as the status
     page to show if the validation is in progress or done and therefore
     conditionally show the validPercent stat.

--- src/lib/web-publisher-info.js
+++ src/lib/web-publisher-info.js
@@ -15,17 +15,39 @@ class  PublisherInfo {
 
     const publisherFeedsSqlQuery = `
      SELECT
+        id as "csFeedId",
         name,
         url
       FROM publisher_feed
       WHERE publisher_id=$1
     `;
 
+    const feedValidationPasses = `
+     SELECT
+       ROUND(100.0 * (SUM(CASE WHEN validation_passed = TRUE THEN 1 ELSE 0 END)::DECIMAL / COUNT(id)), 1) AS "percentValidationPass"
+     FROM raw_data WHERE publisher_feed_id = $1 AND data_deleted = FALSE AND validation_done = TRUE;
+    `;
+
+    const feedValidationCompleted = `
+     SELECT
+       ROUND(100.0 * (SUM(CASE WHEN validation_done = TRUE THEN 1 ELSE 0 END)::DECIMAL / COUNT(id)), 1) AS "percentValidationDone"
+     FROM raw_data WHERE publisher_feed_id = $1 AND data_deleted = FALSE;
+    `;
+
+
     let publisherInfo = {}
 
     try {
       const resPublisher = await client.query(publisherSqlQuery, [publisherId]);
       const resFeeds = await client.query(publisherFeedsSqlQuery, [publisherId]);
+
+      for(let i in resFeeds.rows){
+        const percentValid = await client.query(feedValidationPasses, [resFeeds.rows[i].csFeedId]);
+        const percentValidationDone = await client.query(feedValidationCompleted, [resFeeds.rows[i].csFeedId]);
+
+        resFeeds.rows[i].validPercent = Number(percentValid.rows[0].percentValidationPass);
+        resFeeds.rows[i].validationDonePercent = Number(percentValidationDone.rows[0].percentValidationDone);
+      }
 
       publisherInfo = resPublisher.rows[0];
 
