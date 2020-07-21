4dcf05d237e2ce829003520d02bf647a3ff098cc
rhiaro <amy@rhiaro.co.uk>
2020-07-21 12:44:34 +0200

    db: Add raw_data_parent_id to normalised events

--- src/lib/normalised-event.js
+++ src/lib/normalised-event.js
@@ -4,11 +4,15 @@ import hash from 'object-hash';
 Pipelines can create any number of these as they process data.
 **/
 class NormalisedEvent {
-  constructor(data, kind) {
+  constructor(data, kind, parentId) {
     this.data = data;
     // Is it always possible to extract Kind from data?
     // For now, to be very robust, require the pipe that creates this to set Kind too.
     this.kind = kind;
+    // parentId:
+    // * should be the database id, NOT the id from the actual data
+    // * is not required
+    this.parentId = parentId;
   }
 
   id(){
