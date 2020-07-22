26438ac95e766d3ecb1d8fa0f7be5a2d199aa116
rhiaro <amy@rhiaro.co.uk>
2020-07-22 17:58:29 +0200

    test: add another eventSchedule test

--- test/test-pipe-normalise-schedule.js
+++ test/test-pipe-normalise-schedule.js
@@ -43,4 +43,23 @@ describe('course-schedule', function() {
         assert.deepEqual(results[1].data, output[1].data);
     });
 
+    it('should generate two correct Normalised Events from a SessionSeries with eventSchedule', async function(){
+        const input = await Utils.readJson(path.resolve(path.resolve(), './test/fixtures/sessionseries-with-schedule.json'));
+        const output = await Utils.readJson(path.resolve(path.resolve(), './test/fixtures/sessionseries-with-schedule-normalised.json'));
+        input.data_kind = input.kind;
+        let pipe = new NormaliseSchedulePipe(input, []);
+
+        // Mock eventsFrom and eventsUntil in pipe so it always returns a fixed date, since the dates are hardcoded in 'output' json
+        pipe.eventsFrom = function(){
+            return new Date("2020-07-22T00:00:00.000Z");
+        }
+        pipe.eventsUntil = function(){
+            return new Date("2020-08-05T00:00:00.000Z");
+        }
+
+        let results = await pipe.run();
+        assert.deepEqual(results[0].data, output[0].data);
+        assert.deepEqual(results[1].data, output[1].data);
+    });
+
 });
