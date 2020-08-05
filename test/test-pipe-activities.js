36c6ef00cf5df04b1fda8a578e2f5f35949a6f1d
rhiaro <amy@rhiaro.co.uk>
2020-08-05 11:39:25 +0100

    test: test augmenting activities

--- test/test-pipe-activities.js
+++ test/test-pipe-activities.js
@@ -1,5 +1,6 @@
 import assert from 'assert';
-import { cache } from '../src/lib/utils.js';
+import path from 'path';
+import { cache, Utils } from '../src/lib/utils.js';
 import ActivitiesPipe from '../src/lib/pipes/activities-pipe.js';
 import NormalisedEvent from '../src/lib/normalised-event.js';
 
@@ -72,7 +73,7 @@ describe('activity-id-fixing', function(){
 describe('getting-activity-labels', function(){
 
   it('gets pref labels', function(){
-    cache.activities = {
+    cache.activities.byId = {
       "https://openactive.io/activity-list#7f4": {
         id: "https://openactive.io/activity-list#7f4",
         identifier: "7f4",
@@ -101,7 +102,7 @@ describe('getting-activity-labels', function(){
   });
 
   it('gets alt labels', function(){
-    cache.activities = {
+    cache.activities.byId = {
       "https://openactive.io/activity-list#7f4": {
         id: "https://openactive.io/activity-list#7f4",
         identifier: "7f4",
@@ -125,8 +126,8 @@ describe('getting-activity-labels', function(){
 });
 
 describe('getting-broader-activities', function(){
-  it('should get labels for activities broader in the heirarcy', function(){
-    cache.activities = {
+  it('should get labels for activities broader in the hierarchy', function(){
+    cache.activities.byId = {
       "https://openactive.io/activity-list#f816f3b5-3128-4421-b71a-25cc7c1e1880": {
         "identifier": "f816f3b5-3128-4421-b71a-25cc7c1e1880",
         "type": "Concept",
@@ -165,7 +166,267 @@ describe('getting-broader-activities', function(){
     const pipe = new ActivitiesPipe({},[]);
     let result = pipe.getBroaderActivities("https://openactive.io/activity-list#f816f3b5-3128-4421-b71a-25cc7c1e1880");
     assert.equal(result.length,2);
-    assert.equal(result[0], "Small Sided Football");
-    assert.equal(result[1], "Football");
+    assert.equal(result[0]["prefLabel"], "Small Sided Football");
+    assert.equal(result[1]["prefLabel"], "Football");
   });
+});
+
+describe('augment-activities', function(){
+
+  // Each of these resets the activities cache so it's fetched again
+  // as previous tests modified it
+  // Very occasionally the get request might take too long and a test
+  // times out.. just run them again..
+
+  it('should not drop any non-OA activities', async function(){
+    cache.activities = {};
+    const input = await Utils.readJson(path.resolve(path.resolve(), './test/fixtures/event-normalised.json'));
+    const inputActivity = [
+      {
+        "@id": "https://example.org/activity/running",
+        "@type": "Concept",
+        "inScheme": "https://example.org/activitylist"
+      }
+    ];
+    input.data.description = "asdf!";
+    input.data.activity = [...inputActivity];
+    const normEvent = new NormalisedEvent(input.data, "Event");
+    const pipe = new ActivitiesPipe(input.data, [normEvent]);
+    let result = await pipe.run();
+    assert.deepEqual(result[0].data.activity, inputActivity);
+  });
+
+  it('should not drop activity with id not in OA list', async function(){
+    cache.activities = {};
+    const input = await Utils.readJson(path.resolve(path.resolve(), './test/fixtures/event-normalised.json'));
+    const inputActivity = [
+      {
+        "@id": "https://example.org/activity/running",
+        "@type": "Concept",
+        "prefLabel": "Running"
+      }
+    ];
+    input.data.description = "asdf!";
+    input.data.activity = [...inputActivity];
+    const normEvent = new NormalisedEvent(input.data, "Event");
+    const pipe = new ActivitiesPipe(input.data, [normEvent]);
+    let result = await pipe.run();
+    assert.deepEqual(result[0].data.activity, inputActivity);
+  });
+
+  it('should not drop activity with label not in OA list', async function(){
+    cache.activities = {};
+    const input = await Utils.readJson(path.resolve(path.resolve(), './test/fixtures/event-normalised.json'));
+    const inputActivity = [
+      {
+        "@type": "Concept",
+        "prefLabel": "asdfasdf"
+      }
+    ];
+    input.data.description = "asdf!";
+    input.data.activity = [...inputActivity];
+    const normEvent = new NormalisedEvent(input.data, "Event");
+    const pipe = new ActivitiesPipe(input.data, [normEvent]);
+    let result = await pipe.run();
+    assert.deepEqual(result[0].data.activity, inputActivity);
+  });
+
+  it('should add labels when only id provided', async function(){
+    cache.activities = {};
+    const input = await Utils.readJson(path.resolve(path.resolve(), './test/fixtures/event-normalised.json'));
+    const inputActivity = [
+      {
+        "@id": "https://openactive.io/activity-list#72ddb2dc-7d75-424e-880a-d90eabe91381"
+      }
+    ];
+    const outputActivity = [
+      {
+        "@id": "https://openactive.io/activity-list#72ddb2dc-7d75-424e-880a-d90eabe91381",
+        "@type": "Concept",
+        "prefLabel": "Running",
+        "inScheme": "https://openactive.io/activity-list"
+      }
+    ];
+    input.data.description = "asdf!";
+    input.data.activity = [...inputActivity];
+    const normEvent = new NormalisedEvent(input.data, "Event");
+    const pipe = new ActivitiesPipe(input.data, [normEvent]);
+    let result = await pipe.run();
+    assert.deepEqual(result[0].data.activity, outputActivity);
+  });
+
+  it('should add id when only label provided', async function(){
+    cache.activities = {};
+    const input = await Utils.readJson(path.resolve(path.resolve(), './test/fixtures/event-normalised.json'));
+    const inputActivity = [
+      {
+        "@type": "Concept",
+        "prefLabel": "Running"
+      }
+    ];
+    const outputActivity = [
+      {
+        "@id": "https://openactive.io/activity-list#72ddb2dc-7d75-424e-880a-d90eabe91381",
+        "@type": "Concept",
+        "prefLabel": "Running",
+        "inScheme": "https://openactive.io/activity-list"
+      }
+    ];
+    input.data.description = "asdf!";
+    input.data.activity = [...inputActivity];
+    const normEvent = new NormalisedEvent(input.data, "Event");
+    const pipe = new ActivitiesPipe(input.data, [normEvent]);
+    let result = await pipe.run();
+    assert.deepEqual(result[0].data.activity, outputActivity);
+  });
+
+  it('should add id and set to OA prefLabel when only label provided and it is an altLabel', async function(){
+    cache.activities = {};
+    const input = await Utils.readJson(path.resolve(path.resolve(), './test/fixtures/event-normalised.json'));
+    const inputActivity = [
+      {
+        "@type": "Concept",
+        "prefLabel": "Rambling"
+      }
+    ];
+    const outputActivity = [
+      {
+        "@id": "https://openactive.io/activity-list#95092977-5a20-4d6e-b312-8fddabe71544",
+        "@type": "Concept",
+        "prefLabel": "Walking",
+        "inScheme": "https://openactive.io/activity-list"
+      }
+    ];
+    input.data.description = "asdf!";
+    input.data.activity = [...inputActivity];
+    const normEvent = new NormalisedEvent(input.data, "Event");
+    const pipe = new ActivitiesPipe(input.data, [normEvent]);
+    let result = await pipe.run();
+    assert.deepEqual(result[0].data.activity, outputActivity);
+  });
+
+  it('should add broader activities', async function(){
+    cache.activities = {};
+    const input = await Utils.readJson(path.resolve(path.resolve(), './test/fixtures/event-normalised.json'));
+    const inputActivity = [
+      {
+        "id": "https://openactive.io/activity-list#90346371-03e3-47c8-a25d-07e976b4a4c8",
+        "prefLabel": "Fell Running"
+      }
+    ];
+    const outputActivity = [
+      {
+        "@id": "https://openactive.io/activity-list#90346371-03e3-47c8-a25d-07e976b4a4c8",
+        "@type": "Concept",
+        "prefLabel": "Fell Running",
+        "inScheme": "https://openactive.io/activity-list"
+      },
+      {
+        "@id": "https://openactive.io/activity-list#72ddb2dc-7d75-424e-880a-d90eabe91381",
+        "@type": "Concept",
+        "prefLabel": "Running",
+        "inScheme": "https://openactive.io/activity-list"
+      }
+    ];
+    input.data.description = "asdf!";
+    input.data.activity = [...inputActivity];
+    const normEvent = new NormalisedEvent(input.data, "Event");
+    const pipe = new ActivitiesPipe(input.data, [normEvent]);
+    let result = await pipe.run();
+    assert.deepEqual(result[0].data.activity, outputActivity);
+  });
+
+  it('should extract more activities from description', async function(){
+    cache.activities = {};
+    const input = await Utils.readJson(path.resolve(path.resolve(), './test/fixtures/event-normalised.json'));
+    const inputActivity = [
+      {
+        "@type": "Concept",
+        "prefLabel": "Jogging"
+      }
+    ];
+    const outputActivity = [
+      {
+        "@type": "Concept",
+        "prefLabel": "Jogging"
+      },
+      {
+        "@id": "https://openactive.io/activity-list#72ddb2dc-7d75-424e-880a-d90eabe91381",
+        "@type": "Concept",
+        "prefLabel": "Running",
+        "inScheme": "https://openactive.io/activity-list"
+      }
+    ];
+    input.data.activity = [...inputActivity];
+    const normEvent = new NormalisedEvent(input.data, "Event");
+    const pipe = new ActivitiesPipe(input.data, [normEvent]);
+    let result = await pipe.run();
+    assert.deepEqual(result[0].data.activity, outputActivity);
+  });
+
+  it('should get broader and extract activities from name', async function(){
+    cache.activities = {};
+    const input = await Utils.readJson(path.resolve(path.resolve(), './test/fixtures/event-normalised.json'));
+    delete input.data.activity;
+    const outputActivity = [
+      {
+        "@id": "https://openactive.io/activity-list#90346371-03e3-47c8-a25d-07e976b4a4c8",
+        "@type": "Concept",
+        "prefLabel": "Fell Running",
+        "inScheme": "https://openactive.io/activity-list"
+      },
+      {
+        "@id": "https://openactive.io/activity-list#72ddb2dc-7d75-424e-880a-d90eabe91381",
+        "@type": "Concept",
+        "prefLabel": "Running",
+        "inScheme": "https://openactive.io/activity-list"
+      }
+    ];
+    input.data.name = "Fell running with Tom";
+    input.data.description = "asdf!";
+    const normEvent = new NormalisedEvent(input.data, "Event");
+    const pipe = new ActivitiesPipe(input.data, [normEvent]);
+    let result = await pipe.run();
+    assert.deepEqual(result[0].data.activity, outputActivity);
+  });
+
+  it('should get broader and keep non-OA activities', async function(){
+    cache.activities = {};
+    const input = await Utils.readJson(path.resolve(path.resolve(), './test/fixtures/event-normalised.json'));
+    const inputActivity = [
+      {
+        "@type": "Concept",
+        "prefLabel": "Jogging"
+      },
+      {
+        "id": "https://openactive.io/activity-list#90346371-03e3-47c8-a25d-07e976b4a4c8",
+        "prefLabel": "Fell Running"
+      }
+    ];
+    const outputActivity = [
+      {
+        "@type": "Concept",
+        "prefLabel": "Jogging"
+      },
+      {
+        "@id": "https://openactive.io/activity-list#90346371-03e3-47c8-a25d-07e976b4a4c8",
+        "@type": "Concept",
+        "prefLabel": "Fell Running",
+        "inScheme": "https://openactive.io/activity-list"
+      },
+      {
+        "@id": "https://openactive.io/activity-list#72ddb2dc-7d75-424e-880a-d90eabe91381",
+        "@type": "Concept",
+        "prefLabel": "Running",
+        "inScheme": "https://openactive.io/activity-list"
+      }
+    ];
+    input.data.description = "asdf!";
+    input.data.activity = [...inputActivity];
+    const normEvent = new NormalisedEvent(input.data, "Event");
+    const pipe = new ActivitiesPipe(input.data, [normEvent]);
+    let result = await pipe.run();
+    assert.deepEqual(result[0].data.activity, outputActivity);
+  });
+
 });
