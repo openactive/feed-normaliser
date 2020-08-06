a7a3dcbdc9b7330b8e36b90c59ae0bda58bbc41d
Michael Wood <michael.wood@opendataservices.coop>
2020-08-06 12:02:07 +0100

    web-publisher-info: Remove queries longer needed and return stats obj
    Use the pre-generated stats object instead of generating info on the
    fly.

--- src/lib/web-publisher-info.js
+++ src/lib/web-publisher-info.js
@@ -17,44 +17,22 @@ class  PublisherInfo {
      SELECT
         id as "csFeedId",
         name,
-        url
+        url,
+        stats
       FROM publisher_feed
       WHERE publisher_id=$1
       ORDER BY name
     `;
 
-    const feedValidationPasses = `
-     SELECT
-       ROUND(100.0 * (SUM(CASE WHEN validation_passed = TRUE THEN 1 ELSE 0 END)::DECIMAL / COUNT(id)), 1) AS "percentValidationPass"
-     FROM raw_data WHERE publisher_feed_id = $1 AND data_deleted = FALSE AND validation_done = TRUE;
-    `;
-
-    const feedValidationCompleted = `
-     SELECT
-       ROUND(100.0 * (SUM(CASE WHEN validation_done = TRUE THEN 1 ELSE 0 END)::DECIMAL / COUNT(id)), 1) AS "percentValidationDone"
-     FROM raw_data WHERE publisher_feed_id = $1 AND data_deleted = FALSE;
-    `;
-
-
     let publisherInfo = {}
 
     try {
-      const resPublisher = await client.query(publisherSqlQuery, [publisherId]);
       const resFeeds = await client.query(publisherFeedsSqlQuery, [publisherId]);
+      const resPublisher = await client.query(publisherSqlQuery, [publisherId]);
 
-      for(let i in resFeeds.rows){
-        const percentValid = await client.query(feedValidationPasses, [resFeeds.rows[i].csFeedId]);
-        const percentValidationDone = await client.query(feedValidationCompleted, [resFeeds.rows[i].csFeedId]);
-
-        resFeeds.rows[i].validPercent = Number(percentValid.rows[0].percentValidationPass);
-        resFeeds.rows[i].validationDonePercent = Number(percentValidationDone.rows[0].percentValidationDone);
-      }
-
-      publisherInfo = resPublisher.rows[0];
-
-      if (publisherInfo){
-        publisherInfo.feeds = resFeeds.rows;
-      }
+      publisherInfo.name = resPublisher.rows[0].name;
+      publisherInfo.url = resPublisher.rows[0].url;
+      publisherInfo.feeds = resFeeds.rows;
     } catch(err){
       console.warn(err);
       publisherInfo = false;
