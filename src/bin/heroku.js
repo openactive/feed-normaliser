7e9963056561c7bda79fd6fbb39a1c8744a3501c
Michael Wood <michael.wood@opendataservices.coop>
2020-08-07 17:38:32 +0100

    heroku.js: Make all processes happen synchronously
    As our wider pipeline has dependencies between processes make sure that
    these processes are ordered correctly to avoid any race conditions and
    to improve the deterministic outcome of the data.

--- src/bin/heroku.js
+++ src/bin/heroku.js
@@ -17,14 +17,38 @@ tls.DEFAULT_MIN_VERSION = Settings.tlsDefaultMinimumVersion;
 // This means we can start all our work processes and not worry about stopping them ourselves - Heroku will do that for us!
 // So lets start all processes:
 
-spider(Settings.spiderDataCatalogStartURL);
-download_raw_all_publisher_feeds();
-validate_raw_data_all();
-normalise_data_all_publisher_feeds();
-profile_normalised_data_all();
-PublisherFeedStats.updateAll();
 
-// When a Heroku worker ends, Heroku starts a new one. https://devcenter.heroku.com/articles/dynos#restarting
-// When there is no work to be done, we don't want the worker to be constantly checking as the worker starts, ends, starts, ends, etc in a loop
-// So sleep, so that when there is no work to do we at least only check every X hours
-Utils.sleep("Heroku", 60*60*Settings.herokuWorkerMinimumCycleHours);
+(async() => {
+  console.log("-- Starting spider --");
+  await spider(Settings.spiderDataCatalogStartURL);
+  console.log("-- Done spider --");
+
+  console.log("-- Starting download raw data --");
+  await download_raw_all_publisher_feeds();
+  console.log("-- Done download raw data --");
+
+  console.log("-- Starting validate raw data --");
+  await validate_raw_data_all();
+  console.log("-- Done validate raw data --");
+
+  console.log("-- Starting normalise raw data --");
+  await normalise_data_all_publisher_feeds();
+  console.log("-- Done normalise raw data --");
+
+  console.log("-- Starting profile normalised data --");
+  await profile_normalised_data_all();
+  console.log("-- Done profile normalised data --");
+
+  console.log("-- Starting updating feed stats --");
+  await PublisherFeedStats.updateAll();
+  console.log("-- Done updating feed stats --");
+
+
+  // When a Heroku worker ends, Heroku starts a new one. https://devcenter.heroku.com/articles/dynos#restarting
+  // When there is no work to be done, we don't want the worker to be constantly checking as the worker starts, ends, starts, ends, etc in a loop
+  // So sleep, so that when there is no work to do we at least only check every X hours
+  Utils.sleep("Heroku", 60*60*Settings.herokuWorkerMinimumCycleHours);
+
+
+  process.exit(0);
+})();
