229a1bbab5cca4f3804d0af1771f7306cc0c3aeb
Michael Wood <michael.wood@opendataservices.coop>
2021-02-25 18:28:31 +0000

    normalise/pipeline: Refactor to use async/await and locked transaction
    As with the validator we can run multiple normalisers in parallel if we
    make sure we lock the rows which are currently being normalised to allow
    another process to pick up rows which currently aren't being processed.

--- src/lib/pipeline.js
+++ src/lib/pipeline.js
@@ -1,31 +1,26 @@
 
 class PipeLine {
-  constructor(rawData, pipes, pipeOutputCb) {
+  constructor(rawData, pipes) {
     this.rawData = rawData;
     this.pipes = pipes;
-    this.pipeOutputCb = pipeOutputCb;
   }
 
-  run() {
-    return new Promise(async resolve => {
-      let normalisedEvents = [];
-      let errors = [];
-      for (const Pipe of this.pipes) {
-        let pipeSection;
-        try {
-          pipeSection = new Pipe(this.rawData, normalisedEvents);
-          normalisedEvents = await pipeSection.run();
-          // TODO let the pipeline pass errors back, add them to errors
-        } catch (error) {
-          console.log(`Error running data through pipe ${pipeSection.constructor.name} \n ${error}`);
-          errors.push({ 'error':error, 'pipe':pipeSection.constructor.name});
-        }
+  async run() {
+    let normalisedEvents = [];
+    let errors = [];
+    for (const Pipe of this.pipes) {
+      let pipeSection;
+      try {
+        pipeSection = new Pipe(this.rawData, normalisedEvents);
+        normalisedEvents = await pipeSection.run();
+        // TODO let the pipeline pass errors back, add them to errors
+      } catch (error) {
+        console.log(`Error running data through pipe ${pipeSection.constructor.name} \n ${error}`);
+        errors.push({ 'error':error, 'pipe':pipeSection.constructor.name});
       }
+    }
 
-      await this.pipeOutputCb( this.rawData.id, normalisedEvents, errors);
-
-      resolve("All pipes run");
-    });
+    return { rawDataId: this.rawData.id, normalisedEvents: normalisedEvents, errors: errors};
   }
 
 }
